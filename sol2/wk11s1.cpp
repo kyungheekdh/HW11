0271c3bc05d80325a127ff919dbc81c2047de949
kyungheekdh <151047335+kyungheekdh@users.noreply.github.com>
2023-11-17 15:43:28 +0900

    Update wk11s1.cpp

--- sol2/wk11s1.cpp
+++ sol2/wk11s1.cpp
@@ -22,5 +22,31 @@ int main()
    l4.f1();   
    l4.f2();
 
-   return 0;
+   int A, B, C;
+	float D, E, F, G;
+	int MM[6];
+	float NN[10];
+	ifstream yy;
+	yy.open("annie.dat", ios::binary | ios::in);
+	yy.read((char*)&A, sizeof(int));
+	yy.read((char*)&B, sizeof(int));
+	yy.read((char*)&D, sizeof(float));
+	yy.read((char*)&E, sizeof(float));
+	yy.read((char*)&F, sizeof(float));
+	yy.read((char*)&G, sizeof(float));
+	yy.read((char*)&C, sizeof(int));
+	yy.read((char*)MM, sizeof(int) * 6);
+	yy.read((char*)NN, sizeof(float) * 10);
+	yy.close();
+	cout << "N1 : " << A << "   " << "N2 : " << B << "   " << "a : " << D << endl;
+	cout << "b : " << E << "    " << "x0 : " << F << "    " << "dx : " << G << "   " << "m : " << C << endl;
+	for (int i = 0; i < 6; i += 1) {
+		cout << "i" << i << " : " << MM[i] << endl;
+	}
+	for (int j = 0; j < 10; j += 1) {
+		cout << "j" << j << " : " << NN[j] << endl;
+	}
+	cout << sizeof(LoL);
+	return 10;
 }
+	
