27829b24d804d98d1dd9804e2035e2ba02b2c23b
kyungheekdh <151047335+kyungheekdh@users.noreply.github.com>
2023-11-17 15:52:09 +0900

    Update LoLCha.h

--- sol3/LoLCha.h
+++ sol3/LoLCha.h
@@ -3,54 +3,15 @@
 using namespace std;
 class LoL
 {
-    int* _N1;
-    int* _N2;
-    float* _a;
-    float* _b;
-    float* _x0;
-    float* _dx;
-    int* _m;
+public:
+    LoL(int N1, int N2, float a, float b, float x0, float dx, int m, string ss);
+    ~LoL();
+
+    ofstream dd;
+    int* _N1, * _N2, * _m;
+    float* _a, * _b, * _x0, * _dx;
     string s;
 
-public:
-    LoL(int N1, int N2, float a, float b, float x0, float dx, int m, string ss)
-    {
-        _N1 = new int;
-        *_N1 = N1;
-        _N2 = new int;
-        *_N2 = N2;
-        _a = new float;
-        *_a = a;
-        _b = new float;
-        *_b = b;
-        _x0 = new float;
-        *_x0 = x0;
-        _dx = new float;
-        *_dx = dx;
-        _m = new int;
-        *_m = m;
-        s = ss;
-
-        dd.open(s, ios::binary | ios::out);
-        dd.write((char*)&(*_N1), sizeof(int));
-        dd.write((char*)&(*_N2), sizeof(int));
-        dd.write((char*)&(*_a), sizeof(float));
-        dd.write((char*)&(*_b), sizeof(float));
-        dd.write((char*)&(*_x0), sizeof(float));
-        dd.write((char*)&(*_dx), sizeof(float));
-        dd.write((char*)&(*_m), sizeof(int));
-    }
-    ofstream dd;
     void f1();
     void f2();
-    ~LoL()
-    {
-        delete _N1;
-        delete _N2;
-        delete _a;
-        delete _b;
-        delete _x0;
-        delete _dx;
-        delete _m;
-    }
 };
