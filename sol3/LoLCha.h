0e866b7493cac688c0c00b28e804a25bd55e43cd
kyungheekdh <151047335+kyungheekdh@users.noreply.github.com>
2023-11-17 16:08:08 +0900

    Update LoLCha.h

--- sol3/LoLCha.h
+++ sol3/LoLCha.h
@@ -4,12 +4,14 @@ using namespace std;
 class LoL
 {
 public:
-    LoL(int N1, int N2, float a, float b, float x0, float dx, int m);
+    LoL(int N1, int N2, float a, float b, float x0, float dx, int m, string ss);
+    ~LoL();
+
     ofstream dd;
     int* _N1, * _N2, * _m;
     float* _a, * _b, * _x0, * _dx;
-    void f0(string);
+    string s;
+
     void f1();
     void f2();
-    ~LoL();
 };
