b66117c4b7473066800a29a50d0c5f1bdab6bbe6
kyungheekdh <151047335+kyungheekdh@users.noreply.github.com>
2023-11-17 15:55:06 +0900

    Update  LoLCha.h

--- sol2/ LoLCha.h
+++ sol2/ LoLCha.h
@@ -5,11 +5,14 @@ class LoL
 {
 public:
     LoL(int N1, int N2, float a, float b, float x0, float dx, int m);
-    ~LoL();
     ofstream dd;
     int* _N1,*_N2,* _m;
     float* _a,* _b,* _x0,* _dx;
     void f0(string);
     void f1();
     void f2();
+    ~LoL()
+    {
+        delete _N1, _N2, _a, _b, _x0, _dx, _m;
+    }
 };
