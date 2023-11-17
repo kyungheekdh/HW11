f87af87436efe34028b0b82d2cc61f0fb08f3a4c
kyungheekdh <151047335+kyungheekdh@users.noreply.github.com>
2023-11-17 15:51:44 +0900

    Update LoLCha.cpp

--- sol3/LoLCha.cpp
+++ sol3/LoLCha.cpp
@@ -1,4 +1,29 @@
 #include "LoLCha.h"
+LoL::LoL(int N1, int N2, float a, float b, float x0, float dx, int m, string ss)
+{
+    _N1 = new int; *_N1 = N1;
+    _N2 = new int; *_N2 = N2;
+    _a = new float; *_a = a;
+    _b = new float; *_b = b;
+    _x0 = new float; *_x0 = x0;
+    _dx = new float; *_dx = dx;
+    _m = new int; *_m = m;
+    s = ss;
+
+    dd.open(s, ios::binary | ios::out);
+    dd.write((char*)&(*_N1), sizeof(int));
+    dd.write((char*)&(*_N2), sizeof(int));
+    dd.write((char*)&(*_a), sizeof(float));
+    dd.write((char*)&(*_b), sizeof(float));
+    dd.write((char*)&(*_x0), sizeof(float));
+    dd.write((char*)&(*_dx), sizeof(float));
+    dd.write((char*)&(*_m), sizeof(int));
+}
+
+LoL::~LoL()
+{
+    delete _N1, _N2, _a, _b, _x0, _dx, _m;
+}
 
 void LoL::f1()
 {
